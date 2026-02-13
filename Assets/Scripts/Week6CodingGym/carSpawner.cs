355b66a44999bd7410e73e447a2bd81ee6f39b11
beneteaw <beneteaw@OA-S141-05>
2026-02-13 17:37:06 -0500

    WEEK 6 CODING GYM PT 3 - Frogger 2

--- Assets/Scripts/Week6CodingGym/carSpawner.cs
+++ Assets/Scripts/Week6CodingGym/carSpawner.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 public class carSpawner : MonoBehaviour
 {
-    public GameObject carToSpawn;
-    public int carsToSpawn = 3;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SpawnCars();
+
     }
 
     // Update is called once per frame
@@ -16,14 +15,5 @@ public class carSpawner : MonoBehaviour
         
     }
 
-    void SpawnCars()
-    {
-        Vector3 spawnPosition = Vector3.zero;
-        for (int i = 0; i < carsToSpawn; i++) {
 
-        spawnPosition.x += 2f;
-            spawnPosition.y = Random.Range(-3, 3);
-        Instantiate(carToSpawn, spawnPosition, Quaternion.identity);
-        }
-    }
 }
--- Assets/Scripts/Week6CodingGym/player.cs
+++ Assets/Scripts/Week6CodingGym/player.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,10 +8,18 @@ public class player : MonoBehaviour
 {
     public float playerSpeed = 3;
     private Vector3 spawnPosition;
+
+    public GameObject carToSpawn;
+    public List<GameObject> spawnList;
+    public int carsToSpawn = 3;
+
+    public TextMeshProUGUI winText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spawnPosition = transform.position;
+        SpawnCars();
     }
 
     // Update is called once per frame
@@ -31,11 +42,47 @@ public class player : MonoBehaviour
         {
             oldPosition.y -= playerSpeed * Time.deltaTime;
         }
+
+        if (oldPosition.x >= 5)
+            {
+            winText.text = "Win Frogger";
+            }
+
         transform.position = oldPosition;
+
+        bool carCollision = false;
+        for (int i = 0; i < spawnList.Count; i++)
+        {
+            if (spawnList[i].GetComponent<SpriteRenderer>().bounds.Contains(transform.position))
+            {
+                carCollision = true;
+            }
+            
+        }
+        if (carCollision)
+        {
+            getHit();
+        }
+
+
     }
 
     public void getHit()
     {
         transform.position = spawnPosition;
+    }
+
+    void SpawnCars()
+    {
+        Vector3 spawnPosition = Vector3.zero;
+        for (int i = 0; i < carsToSpawn; i++)
+        {
+
+            spawnPosition.x += 2f;
+            spawnPosition.y = Random.Range(-3, 3);
+
+            GameObject newCar = Instantiate(carToSpawn, spawnPosition, Quaternion.identity);
+            spawnList.Add(newCar);
+        }
     }
 }
